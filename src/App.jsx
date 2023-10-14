b654cd4f04cb190875d91f956aa6c72b9c831000
Shubham Kumar Sah <shubham.6552@gmail.com>
2023-10-14 22:14:21 +0530

    Initial starter template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-9xl text-red-300 font-bold underline">
+        Hello World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
