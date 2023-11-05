bc0675f9fe0f6d325cdde2e8a42d58008d913218
Shubham Kumar Sah <shubham.6552@gmail.com>
2023-11-05 11:16:42 +0530

    create user model

---
+++ server/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: 'string',
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: 'string',
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: 'string',
+        required: true,
+    }
+}, { timestamps: true })
+
+const userModel = new mongoose.Model('User', userSchema);
+
+export default userModel;
