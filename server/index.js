24e9d59d36c6ca99a664f334b73b24239532047c
Shubham Kumar Sah <shubham.6552@gmail.com>
2023-11-05 11:03:04 +0530

    Database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const morgan = require('morgan')
+
 const app = express()
 
-app.listen(5000,() => {
-    console.log("server is running");
+// MIDDLEWARES
+app.use(morgan('dev'))
+
+
+// DATABASE CONNECTIONS
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Database connection established");
+}).catch(error => {
+    console.log(error);
+})
+
+
+app.listen(process.env.PORT,() => {
+    console.log("Server started listening on port " + process.env.PORT);
 })
